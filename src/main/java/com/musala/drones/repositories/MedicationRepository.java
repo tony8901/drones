5483d09608c65a481d909aeb1ce446f6825b4797
@tony.garces89 <tony.garces89@gmail.com>
2023-06-28 21:47:26 -0400

    First commit. Basic entities, repositories and the BasicServices class.

--- src/main/java/com/musala/drones/repositories/MedicationRepository.java
+++ src/main/java/com/musala/drones/repositories/MedicationRepository.java
@@ -1,7 +1,6 @@
 package com.musala.drones.repositories;
 
 import com.musala.drones.entities.Medication;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
